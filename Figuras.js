086289953ff359fcd48b4450b0d36c25f03ab68c
Jesus Salvador Castillo Javier <jesussalvadorcastillo416@gmail.com>
2022-03-03 23:31:54 -0600

    Calcula el valor y lo manda la lavel

--- Figuras.js
+++ Figuras.js
@@ -1,6 +1,10 @@
 
 function calcularCuadrado(){
     var lado=parseInt(document.getElementById("txtLado").value);
-    alert(lado);
-    alert("El area es: "+(lado*lado)+"\nEl perimetro es: "+(lado*4));
+    //alert(lado);
+    //alert("El area es: "+(lado*lado)+"\nEl perimetro es: "+(lado*4));
+    var txtArea=document.getElementById("txtArea");
+    txtArea.innerText="El area es: "+(lado*lado);
+    var txtPerimetro=document.getElementById("txtPerimetro");
+    txtPerimetro.innerText="El perimetro es: "+(lado*4);
 }
